2f342414716bac9275fa036d14b1ebc337b1cf32
vishnu1040 <kvp1040@gmail.com>
2024-08-04 16:00:07 +0530

    hola

---
+++ firstFile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class firstFile
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("The value entered is :"+a);
+        
+    }
+}
